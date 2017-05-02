83a325a403db262f7ff9766885e701eca756743f
Michael Moravec <mail@majkl578.cz>
2017-05-02 17:48:27 +0200

    TestCase: Drop sniff path workaround (fixed upstream)

--- tests/Sniffs/TestCase.php
+++ tests/Sniffs/TestCase.php
@@ -8,12 +8,4 @@ use SlevomatCodingStandard\Sniffs\TestCase as SlevomatTestCase;
 
 abstract class TestCase extends SlevomatTestCase
 {
-    protected function getSniffPath() : string
-    {
-        [$namespace, $sniffName] = explode('.', $this->getSniffName(), 2);
-
-        $className = sprintf('%s\Sniffs\%sSniff', $namespace, strtr($sniffName, '.', '\\'));
-
-        return (new \ReflectionClass($className))->getFileName();
-    }
 }

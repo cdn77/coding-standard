222dd26fa281e0ca4790fcb258712c46f4ad7a77
Simon Podlipsky <simon@podlipsky.net>
2020-10-24 18:34:48 +0200

    Bump phpunit to 9.4

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,5 +3,3 @@
 declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../vendor/squizlabs/php_codesniffer/autoload.php';
-require __DIR__ . '/../vendor/squizlabs/php_codesniffer/src/Util/Tokens.php';

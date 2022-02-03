6a1ed607e3d79c6d8c5da40ff5e670cfdc47fcb5
Simon Podlipsky <simon@podlipsky.net>
2022-02-03 13:16:27 +0100

    Refactor ValidVariableNameSniffTest for better maintenance

--- tests/Sniffs/NamingConventions/ValidVariableNameSniffTest.php
+++ tests/Sniffs/NamingConventions/ValidVariableNameSniffTest.php
@@ -6,6 +6,12 @@ namespace Cdn77\Sniffs\NamingConventions;
 
 use Cdn77\TestCase;
 
+use function array_keys;
+use function is_array;
+use function json_encode;
+
+use const JSON_THROW_ON_ERROR;
+
 class ValidVariableNameSniffTest extends TestCase
 {
     public const CODE_NOT_CAMEL_CAPS = 'NotCamelCaps';
@@ -18,43 +24,59 @@ class ValidVariableNameSniffTest extends TestCase
     {
         $file = self::checkFile(__DIR__ . '/data/ValidVariableNameSniffTest.inc');
 
-        self::assertSame(36, $file->getErrorCount());
+        $errorTypesPerLine = [
+            3 => self::CODE_NOT_CAMEL_CAPS,
+            5 => self::CODE_NOT_CAMEL_CAPS,
+            10 => self::CODE_MEMBER_NOT_CAMEL_CAPS,
+            12 => self::CODE_PUBLIC_HAS_UNDERSCORE,
+            15 => self::CODE_MEMBER_NOT_CAMEL_CAPS,
+            17 => self::CODE_PUBLIC_HAS_UNDERSCORE,
+            20 => self::CODE_MEMBER_NOT_CAMEL_CAPS,
+            22 => self::CODE_PUBLIC_HAS_UNDERSCORE,
+            25 => self::CODE_MEMBER_NOT_CAMEL_CAPS,
+            27 => self::CODE_PRIVATE_NO_UNDERSCORE,
+            31 => self::CODE_NOT_CAMEL_CAPS,
+            33 => self::CODE_NOT_CAMEL_CAPS,
+            36 => self::CODE_STRING_NOT_CAMEL_CAPS,
+            37 => self::CODE_STRING_NOT_CAMEL_CAPS,
+            39 => self::CODE_STRING_NOT_CAMEL_CAPS,
+            42 => self::CODE_NOT_CAMEL_CAPS,
+            44 => self::CODE_NOT_CAMEL_CAPS,
+            53 => self::CODE_MEMBER_NOT_CAMEL_CAPS,
+            58 => self::CODE_MEMBER_NOT_CAMEL_CAPS,
+            62 => self::CODE_MEMBER_NOT_CAMEL_CAPS,
+            63 => self::CODE_NOT_CAMEL_CAPS,
+            64 => self::CODE_NOT_CAMEL_CAPS,
+            67 => self::CODE_NOT_CAMEL_CAPS,
+            81 => self::CODE_STRING_NOT_CAMEL_CAPS,
+            106 => self::CODE_PUBLIC_HAS_UNDERSCORE,
+            107 => [self::CODE_PUBLIC_HAS_UNDERSCORE,self::CODE_MEMBER_NOT_CAMEL_CAPS],
+            108 => self::CODE_PUBLIC_HAS_UNDERSCORE,
+            111 => self::CODE_PRIVATE_NO_UNDERSCORE,
+            112 => self::CODE_PRIVATE_NO_UNDERSCORE,
+            113 => self::CODE_PRIVATE_NO_UNDERSCORE,
+            114 => self::CODE_PRIVATE_NO_UNDERSCORE,
+            123 => self::CODE_PUBLIC_HAS_UNDERSCORE,
+            138 => self::CODE_NOT_CAMEL_CAPS,
+            141 => self::CODE_NOT_CAMEL_CAPS,
+            146 => self::CODE_MEMBER_NOT_CAMEL_CAPS,
+        ];
+        $possibleLines = array_keys($errorTypesPerLine);
 
-        self::assertSniffError($file, 3, self::CODE_NOT_CAMEL_CAPS);
-        self::assertSniffError($file, 5, self::CODE_NOT_CAMEL_CAPS);
-        self::assertSniffError($file, 10, self::CODE_MEMBER_NOT_CAMEL_CAPS);
-        self::assertSniffError($file, 12, self::CODE_PUBLIC_HAS_UNDERSCORE);
-        self::assertSniffError($file, 15, self::CODE_MEMBER_NOT_CAMEL_CAPS);
-        self::assertSniffError($file, 17, self::CODE_PUBLIC_HAS_UNDERSCORE);
-        self::assertSniffError($file, 20, self::CODE_MEMBER_NOT_CAMEL_CAPS);
-        self::assertSniffError($file, 22, self::CODE_PUBLIC_HAS_UNDERSCORE);
-        self::assertSniffError($file, 25, self::CODE_MEMBER_NOT_CAMEL_CAPS);
-        self::assertSniffError($file, 27, self::CODE_PRIVATE_NO_UNDERSCORE);
-        self::assertSniffError($file, 31, self::CODE_NOT_CAMEL_CAPS);
-        self::assertSniffError($file, 33, self::CODE_NOT_CAMEL_CAPS);
-        self::assertSniffError($file, 36, self::CODE_STRING_NOT_CAMEL_CAPS);
-        self::assertSniffError($file, 37, self::CODE_STRING_NOT_CAMEL_CAPS);
-        self::assertSniffError($file, 39, self::CODE_STRING_NOT_CAMEL_CAPS);
-        self::assertSniffError($file, 42, self::CODE_NOT_CAMEL_CAPS);
-        self::assertSniffError($file, 44, self::CODE_NOT_CAMEL_CAPS);
-        self::assertSniffError($file, 53, self::CODE_MEMBER_NOT_CAMEL_CAPS);
-        self::assertSniffError($file, 58, self::CODE_MEMBER_NOT_CAMEL_CAPS);
-        self::assertSniffError($file, 62, self::CODE_MEMBER_NOT_CAMEL_CAPS);
-        self::assertSniffError($file, 63, self::CODE_NOT_CAMEL_CAPS);
-        self::assertSniffError($file, 64, self::CODE_NOT_CAMEL_CAPS);
-        self::assertSniffError($file, 67, self::CODE_NOT_CAMEL_CAPS);
-        self::assertSniffError($file, 81, self::CODE_STRING_NOT_CAMEL_CAPS);
-        self::assertSniffError($file, 106, self::CODE_PUBLIC_HAS_UNDERSCORE);
-        self::assertSniffError($file, 107, self::CODE_PUBLIC_HAS_UNDERSCORE);
-        self::assertSniffError($file, 107, self::CODE_MEMBER_NOT_CAMEL_CAPS);
-        self::assertSniffError($file, 108, self::CODE_PUBLIC_HAS_UNDERSCORE);
-        self::assertSniffError($file, 111, self::CODE_PRIVATE_NO_UNDERSCORE);
-        self::assertSniffError($file, 112, self::CODE_PRIVATE_NO_UNDERSCORE);
-        self::assertSniffError($file, 113, self::CODE_PRIVATE_NO_UNDERSCORE);
-        self::assertSniffError($file, 114, self::CODE_PRIVATE_NO_UNDERSCORE);
-        self::assertSniffError($file, 123, self::CODE_PUBLIC_HAS_UNDERSCORE);
-        self::assertSniffError($file, 138, self::CODE_NOT_CAMEL_CAPS);
-        self::assertSniffError($file, 141, self::CODE_NOT_CAMEL_CAPS);
-        self::assertSniffError($file, 146, self::CODE_MEMBER_NOT_CAMEL_CAPS);
+        $errors = $file->getErrors();
+        foreach ($errors as $line => $error) {
+            self::assertContains($line, $possibleLines, json_encode($error, JSON_THROW_ON_ERROR));
+
+            $errorTypes = $errorTypesPerLine[$line];
+            if (! is_array($errorTypes)) {
+                $errorTypes = [$errorTypes];
+            }
+
+            foreach ($errorTypes as $errorType) {
+                self::assertSniffError($file, $line, $errorType);
+            }
+        }
+
+        self::assertSame(36, $file->getErrorCount());
     }
 }

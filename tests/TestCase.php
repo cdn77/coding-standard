a73ce173632c717ebce8aece105f785a716b2bdb
Simon Podlipsky <simon@podlipsky.net>
2023-04-12 13:36:00 +0200

    chore(deps): directly require slevomat/coding-standard (#109)

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,6 +6,8 @@ namespace Cdn77;
 
 use SlevomatCodingStandard\Sniffs\TestCase as SlevomatTestCase;
 
+use function assert;
+use function class_exists;
 use function str_replace;
 use function strlen;
 use function substr;
@@ -14,6 +16,9 @@ abstract class TestCase extends SlevomatTestCase
 {
     protected static function getSniffClassName(): string
     {
-        return str_replace('\\Tests\\', '\\', substr(static::class, 0, -strlen('Test')));
+        $class = str_replace('\\Tests\\', '\\', substr(static::class, 0, -strlen('Test')));
+        assert(class_exists($class));
+
+        return $class;
     }
 }
